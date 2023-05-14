a3e9cc01b589fb6f099eae8d5890694a619e9267
em <86720055+artemisiaexists@users.noreply.github.com>
2023-05-13 20:20:08 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const fs = require("fs")
 
 let data = [];
